fe60d49cb8c40331b4681d9c1513d934f6ef33ba
TimurIbr <36293367+TimurIbr@users.noreply.github.com>
2019-06-12 18:04:11 +0300

    Update Dual triangle method.py

--- Dual triangle method.py
+++ Dual triangle method.py
@@ -1,10 +1,14 @@
 import numpy as np
 from numpy.linalg import norm
 from scipy.optimize import approx_fprime
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
 
 Y0     = 0
 STEP   = 0.15
-
+A = np.array([[1, 2], [2, 3]])
+b = np.array([1, 2])
 def TriangleDual(target, x0, eps, grad = approx_fprime, step = lambda k: (k+1)/L, alpha = lambda k : 2/(k+1), num_of_steps = 10000):
     index = 0
     y_new = Y0                  
@@ -18,17 +22,27 @@ def TriangleDual(target, x0, eps, grad = approx_fprime, step = lambda k: (k+1)/L
         xk.append(x_new)
         grad_sum += grad(x_new, target, 0.0000001)*step(index)
         y_new = (-1/2*grad_sum)
-        alpha 
         zk.append(alpha(index)*(y_new) + (1 - alpha(index))*zk[index - 1])
         if index >= num_of_steps :
             return zk
         
-A = np.array([[1, 2], [2, 3]])
-b = np.array([1, 2])
-def target_func():
+
+def target_func(x):
     eig, v = np.linalg.eig (A.transpose() @ A) 
     e = min (eig)
     res = (np.linalg.norm((A @ x - b), ord=2))**2 - e*0.5*(np.linalg.norm(x, ord = 2))**2 #
     return res
-    
+
+def gradient (x):
+    eig, v = np.linalg.eig (A.transpose() @ A)
+    eig = min (eig)
+    res = 2*A.transpose() @ (A @ x - b) - x * eig
+    return res
+
+def USAGETriangleDual(Anew = A, Bnew = B, x0 = np.array([0.1, 0.2]), gen_grad = gradient):
+    A = Anew
+    B = Bnew
+    res = OptTriangleDual(target_func, x0, 0.00001, grad = gen_grad)
+    return res
+
 
